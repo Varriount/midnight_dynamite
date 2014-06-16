8651964bad954e0d42fb2147f1aa8939d740c5bd
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2014-06-16 13:25:03 +0200

    Bumps version for v0.2.2 release. Refs #11.

--- midnight_dynamite.nim
+++ midnight_dynamite.nim
@@ -88,10 +88,10 @@ const
     md_ext_lax_spacing, md_ext_no_intra_emphasis,
     md_ext_strikethrough}) ## Default GitHub syntax friendly extension flags.
 
-  version_str* = "0.2.1" ## Version as a string. \
+  version_str* = "0.2.2" ## Version as a string. \
   ## The format is ``digit(.digit)*``.
 
-  version_int* = (major: 0, minor: 2, maintenance: 1) ## \
+  version_int* = (major: 0, minor: 2, maintenance: 2) ## \
   ## Version as an integer tuple.
   ##
   ## Major version changes mean significant new features or a break in
