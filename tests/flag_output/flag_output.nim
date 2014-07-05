e4fcf6e5fa7e6778c5c81bbd24d3a5b4c98cc87f
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2014-07-05 11:23:37 +0200

    Adds HTML embedded test. Refs #4.

--- tests/flag_output/flag_output.nim
+++ tests/flag_output/flag_output.nim
@@ -1,14 +1,38 @@
 import midnight_dynamite, os
 
 type
-  Test_pair = tuple[input, output: string]
+  Test_info = tuple[input, output: string;
+    render_flags: md_render_flags; extension_flags: md_ext_flags]
 
 const
-  test_strings: array[1, Test_pair] = [
-    ("meh", "<p>meh</p>\n"),
+  test_strings: array[2, Test_info] = [
+    ("meh", "<p>meh</p>\n", md_render_flags({}), md_ext_flags({})),
+
+    ("""
+This is a regular paragraph.
+
+<table>
+    <tr>
+        <td>Foo</td>
+    </tr>
+</table>
+
+This is another regular paragraph.
+""", """
+<p>This is a regular paragraph.</p>
+
+<table>
+    <tr>
+        <td>Foo</td>
+    </tr>
+</table>
+
+<p>This is another regular paragraph.</p>
+""",
+md_render_flags({}), md_ext_flags({})),
     ]
 
-proc run_test(pair: Test_pair): bool =
+proc run_test(pair: Test_info): bool =
   ## Makes sure `pair` produces the expected output.
 
   var
