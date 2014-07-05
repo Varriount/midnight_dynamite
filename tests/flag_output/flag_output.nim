09db625da0e7f762b7208c61b5fbfd5c772cfde9
Grzegorz Adam Hankiewicz <gradha@imap.cc>
2014-07-05 10:57:22 +0200

    Starts flag conversion test. Refs #4.

---
+++ tests/flag_output/flag_output.nim
@@ -0,0 +1,70 @@
+import midnight_dynamite, os
+
+type
+  Test_pair = tuple[input, output: string]
+
+const
+  test_strings: array[1, Test_pair] = [
+    ("meh", "<p>meh</p>\n"),
+    ]
+
+proc run_test(pair: Test_pair): bool =
+  ## Makes sure `pair` produces the expected output.
+
+  var
+    RENDER_FLAGS: md_render_flags = {}
+    EXTENSION_FLAGS: md_ext_flags = {}
+    MD_R = init_md_renderer(RENDER_FLAGS)
+    MD_DOC = MD_R.document(EXTENSION_FLAGS)
+    MD_BUFFER = init_md_buffer()
+
+  finally:
+    MD_BUFFER.free
+    MD_DOC.free
+    MD_R.free
+
+  MD_DOC.render(MD_BUFFER, pair.input)
+  let low_level_buffer = $MD_BUFFER
+
+  # Repeat using the convenience proc.
+  var
+    MD_PARAMS = init_md_params(render_flags = RENDER_FLAGS,
+      extension_flags = EXTENSION_FLAGS)
+  finally:
+    MD_PARAMS.free
+
+  let high_level_buffer = MD_PARAMS.render(pair.input)
+
+  if low_level_buffer != high_level_buffer:
+    echo "low level '", low_level_buffer, "' != '", high_level_buffer, "'"
+    return
+  if low_level_buffer != pair.output:
+    echo "low level '", low_level_buffer, "' != '", pair.output, "'"
+    return
+
+  result = true
+
+
+proc run_tests() =
+  ## Iterates over test_strings trying them all and reporting failures.
+  var
+    SUCCESS: seq[int] = @[]
+    FAIL: seq[int] = @[]
+
+  for f, pair in test_strings.pairs:
+    try:
+      if pair.run_test:
+        SUCCESS.add(f)
+        continue
+    except:
+      echo "Severe error running test ", f
+    FAIL.add(f)
+
+  if FAIL.len < 1:
+    echo "All md tests passed."
+  else:
+    echo "Failed ", FAIL.len, " tests out of ", test_strings.len
+    for f in FAIL:
+      echo "\tTest ", f, ": '", test_strings[f].input, "'"
+
+when isMainModule: run_tests()
